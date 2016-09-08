b930b44206a9975843cfaacef0eaacc4d461d48b
Adam Borowski <adam@adamboro.com>
2016-09-08 14:13:03 -0400

    optimise build process

---
+++ webpack.config.dev.js
@@ -0,0 +1,29 @@
+const autoprefixer = require('autoprefixer')
+
+module.exports = {
+  entry: './front/js/main.js',
+  output: {
+    filename: 'public/bundle.js'
+  },
+  resolve: {
+    extensions: ['', '.js', '.sass']
+  },
+  module: {
+    loaders: [
+      {
+        test: /\.js$/,
+        loader: 'babel-loader',
+        query: {
+          presets: ['es2015']
+        }
+      },
+      {
+        test: /\.sass$/,
+        loaders: ['style', 'css', 'postcss-loader', 'sass']
+      }
+    ]
+  },
+  postcss: function () {
+    return [autoprefixer]
+  }
+}
